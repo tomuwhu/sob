43418736d5a461b79ed88e1ff5f0100a7ab532f6
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-20 18:29:24 +0200

    .

---
+++ data.js
@@ -0,0 +1,37 @@
+ds = [{
+        name:   `adatszerkezet`,
+        date:   `2023-07-20`,
+        text:   `Példaprogramokat tároló adatszerkezet: js`,
+        type:   `javascript`,
+        fn:     `./data.js`
+    },
+    {
+        name:   `adatszerkezet`,
+        date:   `2023-07-20`,
+        text:   `Példaprogramokat tároló adatszerkezet: html`,
+        type:   `language-html`,
+        fn:     `./index.html`
+    },
+    {
+        name:   `adatszerkezet`,
+        date:   `2023-07-20`,
+        text:   `Példaprogramokat tároló adatszerkezet: css`,
+        type:   `language-html`,
+        fn:     `./style.css`
+    }]
+s = `<table>${ds.map((e, i) => `<tr>
+    <td>${e.date}</td>
+    <td>${e.name}</td>
+    <td>${e.text}</td>
+    <td onclick='f(${i})' class="bt">Betölt (${e.fn})</td>
+</tr>`).join('')}</table>`
+$(() => {
+    $('#content').html(s)
+})
+function f(i) {
+    $.get(ds[i].fn, data => {
+        $(`#code`).text(data)
+        $(`#code`).className = `ds[i].type`
+        hljs.highlightAll()
+    })
+}
