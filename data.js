8c141924752c010a6bdd2b2c0e563c4bcd75630c
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-20 19:04:18 +0200

    .

--- data.js
+++ data.js
@@ -1,38 +1,35 @@
 ds = [{
-        name:   `adatszerkezet`,
+        name:   `Weboldal forráskód`,
         date:   `2023-07-20`,
-        text:   `Példaprogramokat tároló adatszerkezet: js`,
-        type:   `javascript`,
-        fn:     `data.js`
-    },
-    {
-        name:   `adatszerkezet`,
-        date:   `2023-07-20`,
-        text:   `Példaprogramokat tároló adatszerkezet: html`,
-        type:   `language-html`,
-        fn:     `index.html`
-    },
-    {
-        name:   `adatszerkezet`,
-        date:   `2023-07-20`,
-        text:   `Példaprogramokat tároló adatszerkezet: css`,
-        type:   `language-css`,
-        fn:     `style.css`
+        text:   `Példaprogramokat tároló adatszerkezet, forráskód-megjelenítő`,
+        files: [{
+            type:   `javascript`,
+            fn:     `data.js`
+        }, {
+            type:   `language-html`,
+            fn:     `index.html`
+        }, {
+            type:   `language-css`,
+            fn:     `style.css`
+        }
+        ]
     }]
 s = `<table>${ds.map((e, i) => `<tr>
     <td>${e.date}</td>
     <td>${e.name}</td>
-    <td>${e.text}</td>
-    <td onclick='f(${i})' class="bt">betölt: <i>${e.fn}</i></td>
+    <td class="k1">${e.text}</td>
+    <td><table><td class="bt">betölt:</td>
+    ${e.files.map((fi, j) => `<td onclick='f(${i}, ${j})' class="bt"><i>${fi.fn}</i></td>` ).join('')}
+    </table></td>
 </tr>`).join('')}</table>`
 $(() => {
     $('#content').html(s)
 })
-function f(i) {
-    $.get(ds[i].fn, data => {
+function f(i, j) {
+    $.get(ds[i].files[j].fn, data => {
         $(`#code`).text(data)
         $(`#code`).removeAttr('class')
-        $(`#code`).addClass(ds[i].type)
+        $(`#code`).addClass(ds[i].files[j].type)
         hljs.highlightAll()
     })
 }
