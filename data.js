bad7100673744c5774f3211eb4797baef8c69e35
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-20 18:40:11 +0200

    .

--- data.js
+++ data.js
@@ -3,27 +3,27 @@ ds = [{
         date:   `2023-07-20`,
         text:   `Példaprogramokat tároló adatszerkezet: js`,
         type:   `javascript`,
-        fn:     `./data.js`
+        fn:     `data.js`
     },
     {
         name:   `adatszerkezet`,
         date:   `2023-07-20`,
         text:   `Példaprogramokat tároló adatszerkezet: html`,
         type:   `language-html`,
-        fn:     `./index.html`
+        fn:     `index.html`
     },
     {
         name:   `adatszerkezet`,
         date:   `2023-07-20`,
         text:   `Példaprogramokat tároló adatszerkezet: css`,
         type:   `language-css`,
-        fn:     `./style.css`
+        fn:     `style.css`
     }]
 s = `<table>${ds.map((e, i) => `<tr>
     <td>${e.date}</td>
     <td>${e.name}</td>
     <td>${e.text}</td>
-    <td onclick='f(${i})' class="bt">Betölt (${e.fn})</td>
+    <td onclick='f(${i})' class="bt">betölt: <i>${e.fn}</i></td>
 </tr>`).join('')}</table>`
 $(() => {
     $('#content').html(s)
