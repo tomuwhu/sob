9ccb2972da93f536e1a1966afea4d04796a9d3f2
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-20 20:14:12 +0200

    .

--- data.js
+++ data.js
@@ -19,10 +19,10 @@ s = `<table>${ds.map((e, i) => `<tr>
     <td>${e.date}</td>
     <td>${e.name}</td>
     <td class="k1">${e.text}</td>
-    <td class="git"><a href="${e.git[1]}" target="git">GIT: ${e.git[0]}</a></td>
     <td class="test"><a href="${e.git[2]}" target="git">Próba: ${e.git[0]}</a></td>
     <td><table><td class="bt">Forrás:</td>
     ${e.files.map((fi, j) => `<td onclick='f(${i}, ${j})' class="bt"><i>${fi.fn}</i></td>` ).join('')}
+    <td class="git bt"><a href="${e.git[1]}" target="git">GIT: ${e.git[0]}</a></td>
     </table></td>
 </tr>`).join('')}</table>`
 $(() => {
