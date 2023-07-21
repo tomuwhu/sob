703e4c10df6e7ef31d3f419ca1e0a8e3db71eaef
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-21 12:02:50 +0200

    .

--- data.js
+++ data.js
@@ -14,34 +14,48 @@ ds = [{
             fn:     `style.css`
         }
         ]
-    }]
+    },
+    {   name:   `Répa télapó`,
+        date:   `2023-08-02`,
+        text:   `Karácsonyra megígért rágógumi`},
+    {   name:   `Hangszóró`,
+        date:   `2023-08-02`,
+        text:   `Tüskecumi`}]
 s = x => `${x.map((e, i) => `<div class="c">
     <div class="x">${e.date}</div>
     <div class="x">${e.name}</div>
     <div class="x"><u class="k1">${e.text}</u></div>
     <div><table>
-    <td class="test bt">Próba: <a href="${e.git[2]}" target="git">${e.git[0]}</a></td>
+    ${e.git && e.git[2].length ? `<td class="test bt">Próba: <a href="${e.git[2]}" target="git">${e.git[0]}</a></td>` : ``}
     <td class="bt">Forrás:</td>
-    ${e.files.map((fi, j) => `
+    ${e.files ? e.files.map((fi, j) => `
     <td onclick='f(${i}, ${j}, "${fi.fn}")' class="bt" id="g${i}-${j}"><i>${fi.fn}</i></td>
-    ` ).join('')}
-    <td class="git bt">Git: <a href="${e.git[1]}" target="git">${e.git[0]}</a></td>
+    ` ).join('') : '<td class="bt"> --- <b>Nem listázható</b> --- </td>'} 
+    ${e.git && e.git[1].length ? `<td class="git bt">Git: <a href="${e.git[1]}" target="git">${e.git[0]}</a></td>` : ``}
     </table></div>
     </div>
 `).join('')}`
+
+té = (bsz, hsz1, hsz2) => {
+    lt = new Levenshtein(hsz1, bsz).distance
+    len = hsz1.length
+    lt2 = new Levenshtein(hsz2, bsz).distance
+    len2 = hsz2.length
+    return Math.min(lt/len + lt2/len2)
+}
+
 sz = t => {
     $(`#code`).text("")
-    $('#content').html(s(ds.filter( r => {
+    ds.sort( (r1, r2) => {
         if (!t.value.length) return true
-        lt = new Levenshtein(r.name.toUpperCase(), t.value.toUpperCase()).distance
-        len = r.name.length
-        lt2 = new Levenshtein(r.text.toUpperCase(), t.value.toUpperCase()).distance
-        len2 = r.text.length
-        return (lt/len < 0.9) || (lt2/len2 < 0.92)
-    } )))
+        e1 = té(t.value.toUpperCase(), r1.name.toUpperCase(), r1.text.toUpperCase())
+        e2 = té(t.value.toUpperCase(), r2.name.toUpperCase(), r2.text.toUpperCase())
+        return e1 - e2
+    } )
+    $('#content').html(s(ds.slice(0, 2)))
 }
 $(() => {
-    $('#content').html(s(ds))
+    $('#content').html(s(ds.slice(0, 2)))
 })
 function f(i, j, fn) {
     $.get(fn, data => {
