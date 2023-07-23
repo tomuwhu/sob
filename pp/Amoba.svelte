5c322fe2b77368cb8105df83e6360da1aa0fd072
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-23 18:47:57 +0200

    .

--- pp/Amoba.svelte
+++ pp/Amoba.svelte
@@ -1,11 +1,11 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <script>
-  var n = "O"
-  var t = 
-    Array(20)
+  var n = "O", sx = 30, sy = 20
+  var t = // Üres táblázat
+    Array(sy)
       .fill(0)
       .map(() => 
-        Array(20).fill("")
+        Array(sx).fill("")
       )
 </script>
 
