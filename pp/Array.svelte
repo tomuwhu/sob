1c3ffe197d37f0fe9bdd78656918bcb20763b88c
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-29 09:00:20 +0200

    .

---
+++ pp/Array.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  var t: Array<number> = 
+    Array(10)
+      .fill(0)
+      .map((v, i) => i ** 2)
+  var i: number
+</script>
+
+{#each t as o, i}
+<div><span>{i+1}</span>: <span>{o}</span></div>
+{/each}
+
+<style>
+  div {
+    text-align: right;
+  }
+  span {
+    width: 30px;
+    display: inline-block;
+    background-color: aquamarine;
+    padding: 6px;
+    margin: 6px;
+    box-shadow: 1px 1px 3px black;
+  }
+</style>
