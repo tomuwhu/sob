e55bd620d28787ab3cd1133ffad16c9a13ac0e7d
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-24 07:41:45 +0200

    .

--- pp/Anim2.svelte
+++ pp/Anim2.svelte
@@ -3,14 +3,21 @@
   setInterval(() => (x += x >= -20 ? vx : 1), 10)
 </script>
 
-<h1>Animáció példa</h1>
+<h1>Egyenes vonalú egyenletes mozgás - fizikai szimuláció</h1>
 <button style="left: {x}px;" on:click={() => (vx = vx == 0 ? 0.3 : 0)} />
 <input type="range" bind:value={vx} min={-5} max={5} step={0.1} />
-<br />Sebesség: {vx * 10} pixel/ms
+<br />Sebesség: {Math.trunc(vx * 100)} pixel/s
 
 <style>
   button {
-    position: fixed; top: 56px; width: 20px; height: 20px;
-    background-color: red; cursor: pointer;
+    position: fixed;
+    top: 126px;
+    width: 20px;
+    height: 20px;
+    background-color: red;
+    cursor: pointer;
+  }
+  h1 {
+    font-size: 18px;
   }
 </style>
