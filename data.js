b6aa28bcc608d9b3884927173831a64aec18f775
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-30 12:31:49 +0200

    .

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ n = 5
 a = [-1, -1]
 ds = [
   {
-    name: `SOB_Intranet forrás`,
+    name: `SOB Intranet forráskód`,
     date: `2023-08-29`,
     git: [`SOB`, `https://github.com/tomuwhu/SOB_Intranet/`, `https://tomuwhu.github.io/SOB_Intranet/`],
     keywords: [
