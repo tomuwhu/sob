6d847cbbc99d345b2a6ec30116af4785f6da5793
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-09-06 14:30:26 +0200

    .

--- pp/FileIO.cs
+++ pp/FileIO.cs
@@ -1,8 +1,10 @@
 // Szövegfájl beolvasása
-var l = File.ReadAllLines("file.txt").Select(int.Parse);
+using System.Reflection;
+
+var l = File.ReadAllLines("file.txt").Select(int.Parse).ToArray();
 
 // Elemek száma, összege, szorzata
-var n = l.Count();
+var n = l.Length;
 var sum = l.Sum();
 var prod = l.Aggregate(1, (prod, val) => prod * val);
 Console.WriteLine($"Darabszám: {n} | Összeg: {sum} | Szorzat: {prod}");
@@ -13,23 +15,18 @@ var prod2 = l.Aggregate(1, (p, v) => v % 2 == 0 ? p * v : p);
 var csp = l.Where(v => v % 2 == 0);
 Console.WriteLine($"A páros számok ({String.Join(", ", csp)}) szorzata: {prod2}");
 
-
 Console.WriteLine($"A számok növekvő sorrendben: {String.Join("; ", l.Order())}");
 
 // Számtani közép (átlag)
 var átlag = l.Average();
-var első = l.ElementAt(0);
-var utcsó = l.ElementAt(n - 1);
+var első = l[0];
+var utcsó = l[n - 1];
 Console.WriteLine($"Átlag: {l.Average().ToString("N2")}; Első elem: {első}; Utolsó elem: {utcsó}");
 
 var s = String.Join(", ", l);
 Console.WriteLine($"Min: {l.Min()} | Max: {l.Max()} | Összes elem: {s}");
 
-var al = l.ToArray();
-Console.WriteLine($"A tömb első eleme: {al[0]}");
-
-var l2 = Enumerable.Range(1, 10);
-foreach (var num in l2)
+foreach (var num in new ArraySegment<int>(l, 0, 1))
 {
     Console.Write($"{num}: {Math.Pow(num, 2)} | ");
 }
