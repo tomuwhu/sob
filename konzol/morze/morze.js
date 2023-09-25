0bced59aded2179df4984784c4264bfcffd03b62
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-09-25 10:54:26 +0200

    .

--- konzol/morze/morze.js
+++ konzol/morze/morze.js
@@ -1,14 +1,15 @@
 const fs = require('fs')
 class MorezeMap extends Map {
-    Morze2Szöveg(s) {
-        return s.split("       ").map(v => v.split("   ").map(v => this.get(v)).join("")).join(" ")
-    }
+  Morze2Szöveg = s => s
+    .split("       ").map(v => v.split("   ")
+    .map(v => this.get(v)).join("")).join(" ")
 } 
 decode = new MorezeMap()
 encode = new Map()
-fs  .readFileSync('morzeabc.txt', 'utf8')
-    .split("\n").slice(1).map(v => v.split("\t"))
-    .map(v => (decode.set(v[1], v[0]), v))
-    .forEach(v => encode.set(v[0], v[1]))
-decoded_data = fs.readFileSync('morze.txt', 'utf8').split("\n").map(v => v.split(";").map(v => decode.Morze2Szöveg(v)))
+fs.readFileSync('morzeabc.txt', 'utf8')
+  .split("\n").slice(1).map(v => v.split("\t"))
+  .map(v => (decode.set(v[1], v[0]), v))
+  .forEach(v => encode.set(v[0], v[1]))
+decoded_data = fs.readFileSync('morze.txt', 'utf8').split("\n")
+  .map(v => v.split(";").map(v => decode.Morze2Szöveg(v)))
 console.log(decoded_data)
