eeb22621c2811444da2930ec96f84924a0c64634
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-01-04 11:02:57 +0100

    .

--- brython/mondrian/mondrian.py
+++ brython/mondrian/mondrian.py
@@ -1,82 +1,26 @@
 from browser import document as D, html as H
+from browser import ajax as A
 
-tbl = []
-al = []
-ap = 0
+
+def read(req):
+    global tbl, al
+    dat = list(req.text.split("."))
+    tbl = list(map(lambda x: list(map(int, x)),dat[0].split()))
+    al = list(map(lambda x: list(map(int, str(x[0])+x[1])), enumerate(dat[1].split())))
+    ut()
 
 
 def reset(e):
     global tbl, al, ap
-    if ap > 2: ap = 0
-    tbl = [
-        [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 1, 1, 1, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-        ],
-        [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-        ],
-        [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-        ],
-    ][ap]
-    al = [
-        [
-            (0, 3, 3, 2),
-            (1, 2, 3, 3),
-            (2, 1, 5, 4),
-            (3, 1, 7, 9),
-            (4, 1, 4, 5),
-            (5, 2, 4, 6),
-            (6, 1, 3, 7),
-            (7, 1, 2, 8),
-        ],
-        [
-            (0, 3, 3, 2),
-            (1, 2, 3, 3),
-            (2, 2, 3, 3),
-            (3, 1, 4, 4),
-            (4, 1, 4, 4),
-            (5, 1, 7, 5),
-            (6, 2, 4, 6),
-        ],
-        [
-            (0, 3, 3, 2),
-            (1, 2, 3, 3),
-            (2, 1, 5, 4),
-            (3, 1, 7, 9),
-            (4, 1, 4, 5),
-            (5, 2, 4, 6),
-            (6, 1, 3, 7),
-            (7, 1, 2, 8),
-        ],
-    ][ap]
-    ut()
-
-
-T = H.DIV()
-
+    if ap > 2:
+        ap = 0
+    A.get(f"p{ap}.dat", oncomplete=read)
+    
 
 def rotate(e):
-    obj = al[int(e.target.id)]
-    al[int(e.target.id)] = (obj[0], obj[2], obj[1], obj[3])
+    id = int(e.target.id)
+    obj = al[id]
+    al[id] = (obj[0], obj[2], obj[1], obj[3])
     ut()
 
 
@@ -86,7 +30,6 @@ def ds(e):
     rect = e.target.getBoundingClientRect()
     dy = rect.top - e.y
     dx = rect.left - e.x
-    print(dx, dy, me)
 
 
 def de(e):
@@ -97,8 +40,11 @@ def de(e):
     py = int((e.y + dy - trect.top + 20) // 34)
     rx = obj[1]
     ry = obj[2]
+    if px + rx > 7:
+        return
+    if py + ry > 7:
+        return
     o = 0
-    print(px, py, me, rx, ry)
     for ix in range(rx):
         for iy in range(ry):
             o += tbl[py + iy][px + ix]
@@ -111,6 +57,7 @@ def de(e):
     if len(list(filter(lambda x: x[3], al))) == 0:
         ap += 1
 
+
 def do(e):
     e.preventDefault()
     return True
@@ -119,19 +66,13 @@ def do(e):
 def ut():
     global t
     T.clear()
-    t = H.TABLE(
-        [
-            H.TR([H.TD(Class=f"x{e}") for (i, e) in enumerate(row)])
-            for (j, row) in enumerate(tbl)
-        ]
-    )
-
+    t = H.TABLE([H.TR([H.TD(Class=f"x{e}") for e in row]) for row in tbl])
     T <= H.DIV(t.bind("drop", de).bind("dragover", do))
     T <= H.DIV(
         H.DIV(
             Class=f"al al{c}",
             id=f"{i}",
-            draggable=f"{not c==0}",
+            draggable="true",
             style={"width": f"{34*x}px", "height": f"{34*y}px"},
         )
         .bind("click", rotate)
@@ -140,6 +81,10 @@ def ut():
     )
 
 
+tbl = []
+al = []
+ap = 2
+T = H.DIV()
 D <= H.H1("Mondrian")
 D <= T
 D <= H.BUTTON("Reset").bind("click", reset)
